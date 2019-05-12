533dfc0da2a388597169c35bac7a9c69a6f6443f
Riccardo Porcù <riccardoporc@gmail.com>
2019-05-12 23:54:55 +0200

    .csv file name changed

--- csv_email_extractor.py
+++ csv_email_extractor.py
@@ -2,7 +2,7 @@ import csv
 
 res = ''
 
-with open('prato.csv', mode='r') as csv_file:
+with open('results.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for line in csv_reader:
         if line[3] != '':
